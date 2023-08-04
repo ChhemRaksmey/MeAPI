abbada845371e1c56adabde43bd8bfd6b99cc7ee
CHHEM RAKSMEY <raksmey.chhem@dpbank.local>
2023-08-04 13:26:47 +0700

    update

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -10,42 +10,47 @@ class UserLoginController extends Controller
 {
     function login(Request $requst){
 
-        $ERR_MSG = array();
+        return \Response::json(array(
+            "header" => "asdas",
+            "body" => $requst->all(),
+        ));
 
-        if(!isset($requst->TOOKEN) || empty($requst->TOOKEN)) array_push($ERR_MSG, "Token Missing");
-        if(!isset($requst->DEVICE_IP) || empty($requst->DEVICE_IP)) array_push($ERR_MSG, "Device IP Address Missing");
-        if(!isset($requst->DEVICE_MAC) || empty($requst->DEVICE_MAC)) array_push($ERR_MSG, "Device Mac Address Missing");
-        if(!isset($requst->USER_ID) || empty($requst->USER_ID)) array_push($ERR_MSG, "Signon Name Missing");
-        if(!isset($requst->USER_PASSWORD) || empty($requst->USER_PASSWORD)) array_push($ERR_MSG, "Password Missing");
+        // $ERR_MSG = array();
+
+        // if(!isset($requst->TOOKEN) || empty($requst->TOOKEN)) array_push($ERR_MSG, "Token Missing");
+        // if(!isset($requst->DEVICE_IP) || empty($requst->DEVICE_IP)) array_push($ERR_MSG, "Device IP Address Missing");
+        // if(!isset($requst->DEVICE_MAC) || empty($requst->DEVICE_MAC)) array_push($ERR_MSG, "Device Mac Address Missing");
+        // if(!isset($requst->USER_ID) || empty($requst->USER_ID)) array_push($ERR_MSG, "Signon Name Missing");
+        // if(!isset($requst->USER_PASSWORD) || empty($requst->USER_PASSWORD)) array_push($ERR_MSG, "Password Missing");
 
 
-        if(sizeof($ERR_MSG)>0){
+        // if(sizeof($ERR_MSG)>0){
 
-            return \Response::json(array(
-                "header" => [
-                    "status" => "FAIL",
-                    "code" => "4101",
-                    "tooken" => "",
-                    "message" => $requst->all(),
-                    "response" => $ERR_MSG
-                ],
-            ));
+        //     return \Response::json(array(
+        //         "header" => [
+        //             "status" => "FAIL",
+        //             "code" => "4101",
+        //             "tooken" => "",
+        //             "message" => "",
+        //             "response" => $ERR_MSG
+        //         ],
+        //     ));
 
-        }else{
+        // }else{
             
-            $USER_INFO = DB::TABLE("C_USER")->FIRST();
+        //     $USER_INFO = DB::TABLE("C_USER")->FIRST();
 
-            return \Response::json(array(
-                "header" => [
-                    "status" => "OK",
-                    "code" => "",
-                    "tooken" => "",
-                    "message" => "",
-                ],
-                "body" => $USER_INFO
-            ));
+        //     return \Response::json(array(
+        //         "header" => [
+        //             "status" => "OK",
+        //             "code" => "",
+        //             "tooken" => "",
+        //             "message" => "",
+        //         ],
+        //         "body" => $USER_INFO
+        //     ));
 
-        }
+        // }
 
     }
 
